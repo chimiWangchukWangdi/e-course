7cf2c0d47643ed60b13aed660b9ea252547fda02
chimiWangchukWangdi <chimiwangchukw@gmail.com>
2022-11-24 18:50:20 +0600

    build fixes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDR2BBLm4rFnaojLDL4QMznNS_lqrN83x8",
+    authDomain: "abhyaasproject-44d6d.firebaseapp.com",
+    databaseURL: "https://abhyaasproject-44d6d-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "abhyaasproject-44d6d",
+    storageBucket: "abhyaasproject-44d6d.appspot.com",
+    messagingSenderId: "825093763416",
+    appId: "1:825093763416:web:2c983b961f22e51f4f752d",
+    measurementId: "G-CREY03J897"
+  }
 };
